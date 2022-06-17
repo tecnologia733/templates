6e17cc7a8cbd336f84fce83f9eda984336471d82
tecnologia <77143340+tecnologia733@users.noreply.github.com>
2022-06-17 19:51:49 -0300

    Update custom.js

--- alive-responsive-coming-soon-template/js/custom.js
+++ alive-responsive-coming-soon-template/js/custom.js
@@ -7,7 +7,7 @@ Theme by: WebThemez.com
 Note: Please use our back link in your site
 */
 $( function() {
-        var endDate = "October  02, 2022 15:03:25";
+        var endDate = "October 02,2022 15:03:25";
 
         $('.countdown.simple').countdown({ date: endDate });
 
