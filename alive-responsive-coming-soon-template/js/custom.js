aa16ef6a7cf61158b356869897d804b460736461
tecnologia <77143340+tecnologia733@users.noreply.github.com>
2022-06-17 19:27:13 -0300

    Update custom.js

--- alive-responsive-coming-soon-template/js/custom.js
+++ alive-responsive-coming-soon-template/js/custom.js
@@ -14,7 +14,7 @@ $( function() {
         $('.countdown.styled').countdown({
           date: endDate,
           render: function(data) {
-            $(this.el).html("<div>" + this.leadingZeros(data.days, 3) + " <span>days</span></div><div>" + this.leadingZeros(data.hours, 2) + " <span>hrs</span></div><div>" + this.leadingZeros(data.min, 2) + " <span>min</span></div><div>" + this.leadingZeros(data.sec, 2) + " <span>sec</span></div>");
+            $(this.el).html("<div>" + this.leadingZeros(data.days, 3) + " <span>days</span></div><div>" + this.leadingZeros(data.hours, 4) + " <span>hrs</span></div><div>" + this.leadingZeros(data.min, 6) + " <span>min</span></div><div>" + this.leadingZeros(data.sec, 7) + " <span>sec</span></div>");
           }
         });
 
